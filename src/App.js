75dc6dce2138a2dfd0b75a128c5ca11d736712c0
zhanyl <turtemirova.zhanyl@gmail.com>
2021-02-13 14:12:49 +0600

    task-1: components creation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
+class ClassComponent extends Component {
+  render() {
+    return <div>Class Component</div>;
+  }
+}
+
+function FunctionalComponent() {
+  return <div>Functional Component</div>;
+}
+
+const createComponent = () => {
+  let el = React.createElement(
+    "div",
+    { style: { color: "blue" } },
+    `Create component using 'React.createElement'`
+  );
+
+  ReactDOM.render(el, document.querySelector("#example"));
+};
+class PureComponentExample extends React.PureComponent {
+  render() {
+    return <div>Pure Component</div>;
+  }
+}
+const HelloMessage = () => {
+  return <div>Hello React!</div>;
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {createComponent()}
+      <ClassComponent />
+      <PureComponentExample />
+      <FunctionalComponent />
+      <HelloMessage />
     </div>
   );
 }
